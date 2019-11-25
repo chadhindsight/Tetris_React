259a4c857461bcbae39a51498d7064b1cd99e183
Chad Hinds <chadrickj8@gmail.com>
2019-11-25 17:25:05 -0500

    Import all game components into Tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,1 +1,22 @@
 import React from 'react';
+// Components
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+const Tetris = () => {
+   return ( <div>
+        <Stage />
+        <aside>
+           <div>
+                <Display text="Score" />
+                <Display text="Rows" />
+                <Display text="Levels" />
+           </div>
+           <Start/>
+        </aside>
+    </div>
+   )
+};
+
+export default Tetris;
