3ee37b5d2e08d4bb093190067cf1dbce0fcac47a
Chad Hinds <chadrickj8@gmail.com>
2019-11-30 20:44:08 -0500

    use stage height to apply hieght for  each row that we have

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,7 +1,7 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
-//Create a new array with the cells for each row that we have.
+//Create a new array with the cells for each row that we have
 export const createStage = () =>
 Array.from(Array(STAGE_HEIGHT), () =>{
     new Array(STAGE_WIDTH).fill([0,'clear'])
