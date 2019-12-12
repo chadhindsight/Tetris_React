1ce2ce87746f807c68153cc559a6b3efd80126da
Chad Hinds <chadrickj8@gmail.com>
2019-12-12 16:44:19 -0500

    Rework function

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,8 +1,8 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
-//Create a new array with the cells for each row that we have
+//Create a new array with the cells for each row
 export const createStage = () =>
-Array.from(Array(STAGE_HEIGHT), () =>{
-    new Array(STAGE_WIDTH).fill([0,'clear'])
-})
+Array.from(Array(STAGE_HEIGHT), () =>(
+    new Array(STAGE_WIDTH).fill[0, 'clear'])
+)
