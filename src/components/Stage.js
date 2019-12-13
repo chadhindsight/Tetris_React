797dafa39f5d10b714578befd8ef8bf1939b3aa0
Chad Hinds <chadrickj8@gmail.com>
2019-12-13 18:27:08 -0500

    debug the function that displays cells

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Cell from './Cell';
-
-const Stage = ({ stage }) => (
-    <div><Cell /></div>
+// I'm trash
+const Stage = ( {stage} ) => (
+    <div>
+        {stage.map(row => row.map((cell, x)=> <Cell key={x} type={cell[0]}/>))}
+    </div>
 )
 export default Stage;
