c50d956d7bbbc1fd9f2a95d52f30013e76ca52ff
Chad Hinds <chadrickj8@gmail.com>
2020-01-01 10:09:14 -0500

    Remove comment that was not needed

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createStage  } from '../gameHelpers';
-// Components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
-
+// Tetris Component
 const Tetris = () => {
    return ( <div>
         <Stage stage={createStage()}/>
